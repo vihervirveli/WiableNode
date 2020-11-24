629f360c1c65d21742bf39ba4c1f6bda9f0b946a
Maarit <p0033@student.jamk.fi>
2020-11-24 09:53:52 +0200

    reconfig winston

--- winston_config.js
+++ winston_config.js
@@ -1,13 +1,40 @@
-const winston = require('winston');
+var appRoot = require('app-root-path');
+var winston = require('winston');
 
-//const myFormat = winston.format.cli({colors:{info: 'blue'}})
-const logger = winston.createLogger({
-    transports: [
-//        new winston.transports.Console({
-//            format: myFormat
-//        }),
-        new (winston.transports.File) ({filename: 'test.log'})
-    ]
+// define the custom settings for each transport (file, console)
+var options = {
+  file: {
+    level: 'info',
+    filename: `${appRoot}/logs/app.log`,
+    handleExceptions: true,
+    json: true,
+    maxsize: 5242880, // 5MB
+    maxFiles: 5,
+    colorize: false,
+  },
+  console: {
+    level: 'debug',
+    handleExceptions: true,
+    json: false,
+    colorize: true,
+  },
+};
+
+// instantiate a new Winston Logger with the settings defined above
+var logger = new winston.Logger({
+  transports: [
+    new winston.transports.File(options.file),
+    new winston.transports.Console(options.console)
+  ],
+  exitOnError: false, // do not exit on handled exceptions
 });
 
-module.exports = logger
+// create a stream object with a 'write' function that will be used by `morgan`
+logger.stream = {
+  write: function(message, encoding) {
+    // use the 'info' log level so the output will be picked up by both transports (file and console)
+    logger.info(message);
+  },
+};
+
+module.exports = logger;
