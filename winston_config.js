89f7e34d822bc852ff559e13a52ad955f1be3027
Maarit <p0033@student.jamk.fi>
2020-11-24 12:33:57 +0200

    winston config uusiksi

--- winston_config.js
+++ winston_config.js
@@ -1,40 +1,29 @@
-let appRoot = require('app-root-path');
-let winston = require('winston');
+const winston = require('winston');
+const logDir = 'logs';
+const fs = require('fs');
 
-// define the custom settings for each transport (file, console)
-let options = {
-  file: {
+if (!fs.existsSync(logDir)) {
+    fs.mkdirSync(logDir);
+}
+
+const logger = winston.createLogger({
     level: 'info',
-    filename: `${appRoot}/logs/app.log`,
-    handleExceptions: true,
-    json: true,
-    maxsize: 5242880, // 5MB
-    maxFiles: 5,
-    colorize: false,
-  },
-  console: {
-    level: 'debug',
-    handleExceptions: true,
-    json: false,
-    colorize: true,
-  },
-};
-
-// instantiate a new Winston Logger with the settings defined above
-let logger = winston.createLogger({
-  transports: [
-    new winston.transports.File(options.file),
-    new winston.transports.Console(options.console)
-  ],
-  exitOnError: false, // do not exit on handled exceptions
+    format: winston.format.json(),
+    exitOnError: false,
+    transports: [
+        new (require('winston-daily-rotate-file'))({
+            filename: `${logDir}/logs.log`,
+            datePattern: 'YYYY-MM-DD-HH',
+            zippedArchive: true,
+            level: 'silly'
+        }),
+        new (require('winston-daily-rotate-file'))({
+            filename: `${logDir}/errors.log`,
+            datePattern: 'YYYY-MM-DD-HH',
+            zippedArchive: true,
+            level: 'error'
+        })
+    ],
 });
 
-// create a stream object with a 'write' function that will be used by `morgan`
-logger.stream = {
-  write: function(message, encoding) {
-    // use the 'info' log level so the output will be picked up by both transports (file and console)
-    logger.info(message);
-  },
-};
-
-module.exports = logger;
+export {logger};
