cf0d5456a307e631047a436d30b1ba8e6870b4f0
Maarit <p0033@student.jamk.fi>
2020-11-24 15:54:16 +0200

    env

--- app.js
+++ app.js
@@ -34,7 +34,7 @@ if (!isProduction) {
 if(isProduction){
   mongoose.connect(process.env.MONGODB_URI);
 } else {
-  mongoose.connect('mongodb://localhost/conduit');
+  mongoose.connect(process.env.MONGODB_URI);
   mongoose.set('debug', true);
 }
 
