db89b9a0b3c573f670e953d2dcdb4340f32d5c98
Maarit <p0033@student.jamk.fi>
2020-11-25 13:42:32 +0200

    log stuff in users.js

--- winston_config.js
+++ winston_config.js
@@ -26,30 +26,3 @@ const logger = createLogger({
   ]
 });
 module.exports = logger
-/*
-
-const winston = require('winston');
-const logDir = 'logs';
-const fs = require('fs');
-
-if (!fs.existsSync(logDir)) {
-    fs.mkdirSync(logDir);
-}
-
-const logger = winston.createLogger({
-    level: 'info',
-    format: winston.format.json(),
-    exitOnError: false,
-    transports: [
-        new (require('winston-daily-rotate-file'))({
-            filename: `${logDir}/app.log`,
-            datePattern: 'YYYY-MM-DD-HH',
-            zippedArchive: true,
-            level: 'info'
-        })
-    ],
-});
-
-module.exports = logger
-
-*/
